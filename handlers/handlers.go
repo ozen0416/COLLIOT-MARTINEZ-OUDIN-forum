d9a69f4c549f79ab09f9766d7f4b3622a4c28128
cotheo <theo.colliot@ynov.com>
2023-06-05 13:55:51 +0200

    Ajout des templates dans le main.go + debout de la route Login

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -24,19 +28,40 @@ func HandleNotFound(files []string, w http.ResponseWriter, _ *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-//func HandleLogin(files []string) {
-//	http.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
-//		if request.URL.Path != "login" {
-//			HandleNotFound(files, writer, request)
-//			return
-//		}
-//		if request.Method != http.MethodPost {
-//			f := append(files, "templates/login.html")
-//			tmpl := template.Must(template.ParseFiles(f...))
-//			tmpl.Execute(w, nil)
-//			return
-//		}
-//
-//	})
-//
-//}
+func HandleLogin(files []string) {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/forum")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM `topic` ORDER BY topic.publi_date DESC")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
+		if request.URL.Path != "/login" {
+			HandleNotFound(files, writer, request)
+			return
+		}
+		for results.Next() {
+			var content int
+			results.Scan(&content)
+			fmt.Println(content)
+		}
+
+		f := append(files, "templates/dua.html")
+		tmpl := template.Must(template.ParseFiles(f...))
+		tmpl.Execute(writer, nil)
+
+		//if request.Method != http.MethodPost {
+		//	f := append(files, "templates/login.html")
+		//	tmpl := template.Must(template.ParseFiles(f...))
+		//	tmpl.Execute(w, nil)
+		//	return
+		//}
+
+	})
+
+}
--- main.go
+++ main.go
@@ -10,10 +10,15 @@ const port = ":8080"
 
 func main() {
 
-	f := []string{"templates/index.html"}
+	f := []string{
+		"templates/index.html",
+		"templates/header.html",
+		"templates/footer.html"
+	}
 
 	fmt.Println("(http://localhost:8080)", "Server started on port ", port)
 
 	handlers.HandleIndex(f)
-	http.ListenAndServe(":8080", nil)
+	handlers.HandleLogin(f)
+	http.ListenAndServe(port, nil)
 }
