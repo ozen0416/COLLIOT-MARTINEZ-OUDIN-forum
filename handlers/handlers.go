24cc499fa3a53d5a2ce2dd82b63dde9724a2d206
cotheo <theo.colliot@ynov.com>
2023-06-06 11:15:42 +0200

    Ajout de cookies de session + route /logout + affichage de la route /dashboard seulement quand connecté + affichage de donnée sur la route / quand connecté

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,35 +1,57 @@
 package handlers
 
 import (
+	"crypto/rand"
 	"database/sql"
-	"forum/structures"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/sessions"
+	_ "github.com/gorilla/sessions"
 	"html/template"
 	"log"
 	"net/http"
 )
 
+var (
+	key   = make([]byte, 64)
+	_, _  = rand.Read(key)
+	store = sessions.NewCookieStore(key)
+)
+
 func HandleIndex(files []string) {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			HandleNotFound(files, w, r)
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		session, _ := store.Get(request, "cookie-forum-ynov")
+
+		if request.URL.Path != "/" {
+			HandleNotFound(files, writer, request)
 			return
 		}
 		f := append(files, "templates/landing-page.html")
 		tmpl := template.Must(template.ParseFiles(f...))
-		tmpl.Execute(w, nil)
+
+		// If not authenticated
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			tmpl.Execute(writer, nil)
+			return
+		}
+		// If authenticated
+		id := session.Values["id-user"].(int)
+		tmpl.Execute(writer, struct {
+			Id   int
+			Mess string
+		}{Id: id, Mess: "authentifié est mon id est : "})
 	})
 }
 
-func HandleNotFound(files []string, w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(404)
+func HandleNotFound(files []string, writer http.ResponseWriter, _ *http.Request) {
+	writer.WriteHeader(404)
 	f := append(files, "templates/not-found.html")
 	tmpl := template.Must(template.ParseFiles(f...))
-	tmpl.Execute(w, nil)
+	tmpl.Execute(writer, nil)
 }
 
 func HandleLogin(files []string) {
 	http.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
+		session, _ := store.Get(request, "cookie-forum-ynov")
 		if request.URL.Path != "/login" {
 			HandleNotFound(files, writer, request)
 			return
@@ -51,9 +73,9 @@ func HandleLogin(files []string) {
 		email := request.FormValue("email")
 		pass := request.FormValue("pass")
 
-		var authValue structures.Auth
+		var idUser int
 
-		err = db.QueryRow("SELECT email, passwd FROM `users` WHERE users.email = ? AND users.passwd = ?", email, pass).Scan(&authValue.Email, &authValue.Pass)
+		err = db.QueryRow("SELECT id FROM `users` WHERE users.email = ? AND users.passwd = ?", email, pass).Scan(&idUser)
 		if err != nil {
 			f := append(files, "templates/login.html")
 			tmpl := template.Must(template.ParseFiles(f...))
@@ -61,6 +83,9 @@ func HandleLogin(files []string) {
 			return
 		}
 
+		session.Values["authenticated"] = true
+		session.Values["id-user"] = idUser
+		session.Save(request, writer)
 		http.Redirect(writer, request, "/dashboard", 302)
 
 	})
@@ -69,9 +94,23 @@ func HandleLogin(files []string) {
 
 func HandleDashboard(files []string) {
 	http.HandleFunc("/dashboard", func(writer http.ResponseWriter, request *http.Request) {
+		session, _ := store.Get(request, "cookie-forum-ynov")
+
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			HandleNotFound(files, writer, request)
+			return
+		}
+
 		f := append(files, "templates/dua.html")
 		tmpl := template.Must(template.ParseFiles(f...))
 		tmpl.Execute(writer, nil)
 		return
+	})
+}
+
+func HandlerLogout() {
+	http.HandleFunc("/logout", func(writer http.ResponseWriter, request *http.Request) {
+		store.MaxAge(-1)
+		http.Redirect(writer, request, "/", 302)
 	})
 }
--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 
 	handlers.HandleIndex(f)
 	handlers.HandleLogin(f)
+	handlers.HandlerLogout()
 	handlers.HandleDashboard(f)
 	http.ListenAndServe(port, nil)
 }
