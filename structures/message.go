42145c5d9e77e7d444670549a52ed590347fe59d
cotheo <theo.colliot@ynov.com>
2023-06-15 00:47:45 +0200

    modif des requetes sql pour la structure Message

--- structures/message.go
+++ structures/message.go
@@ -9,19 +9,19 @@ import (
 type Message struct {
 	Id        int
 	Content   string
-	AuthorId  int
+	Author    User
 	TopicId   int
 	PubliDate string
 }
 
 func SendMess(m Message) (*sql.Rows, error) {
 	db := database.ReturnDatabase()
-	return db.Query("INSERT INTO `message`(`content`, `user_id`, `topic_id`) VALUES (?,?,?)", m.Content, m.AuthorId, m.TopicId)
+	return db.Query("INSERT INTO `message`(`content`, `user_id`, `topic_id`) VALUES (?,?,?)", m.Content, m.Author, m.TopicId)
 }
 
 func GetMessByTopicId(id string) []Message {
 	db := database.ReturnDatabase()
-	rows, err := db.Query("select * from `message` where topic_id = ? order by DatePost DESC", id)
+	rows, err := db.Query("SELECT message.id, message.content, message.user_id, message.DatePost, message.topic_id, users.nickname FROM `message` INNER JOIN `users` on message.user_id = users.id WHERE message.topic_id = ? ORDER BY message.DatePost DESC", id)
 	if err != nil {
 		fmt.Println("GetMessByTopicId: ", err)
 	}
@@ -29,7 +29,7 @@ func GetMessByTopicId(id string) []Message {
 	var messageArray []Message
 	for rows.Next() {
 		var messcToAppend Message
-		err := rows.Scan(&messcToAppend.Id, &messcToAppend.Content, &messcToAppend.AuthorId, &messcToAppend.PubliDate, &messcToAppend.TopicId)
+		err := rows.Scan(&messcToAppend.Id, &messcToAppend.Content, &messcToAppend.Author.Id, &messcToAppend.PubliDate, &messcToAppend.TopicId, &messcToAppend.Author.Username)
 		if err != nil {
 			fmt.Println("Scan through topics", err)
 		}
